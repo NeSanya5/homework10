3fe69c58b6979493a7d90896a3158ab555f1fcb4
Шороховы <soroho78@gmail.com>
2025-01-01 22:53:42 +0300

    homework10

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
         String firstName = "Ivan ";
-        String middleName = "Ivanov ";
-        String lastName = "Ivanovich ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
         String fullName = lastName + firstName + middleName;
         System.out.println("Ф. И. О. сотрудника — " + fullName);
         System.out.println("Задание 2");
