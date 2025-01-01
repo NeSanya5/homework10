ee45e299a470ce28a3a637d9bcb468d134cfc538
Шороховы <soroho78@gmail.com>
2025-01-01 22:58:03 +0300

    homework10

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        String firstName = "Ivan ";
-        String middleName = "Ivanovich ";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleName;
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Ф. И. О. сотрудника — " + fullName);
         System.out.println("Задание 2");
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
