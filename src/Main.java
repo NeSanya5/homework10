81576859345892ef8bca2fcd9a8e4906ed4626ae
Шороховы <soroho78@gmail.com>
2025-01-01 21:04:44 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Задание 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanov ";
+        String lastName = "Ivanovich ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("Задание 2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println("Задание 3");
+        String fullName2 = "Иванов Семён Семёнович";
+        fullName2 = fullName2.replace("ё" , "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName2);
+
     }
 }
